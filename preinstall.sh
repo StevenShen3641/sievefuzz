318ab75b8c1cfa34217984fa21670a3911cab86a
StevenShen3641 <stevenshen3641@outlook.com>
2025-05-31 16:25:22 +0000

    ensure objcopy compatibility

--- preinstall.sh
+++ preinstall.sh
@@ -14,3 +14,18 @@ update-alternatives --install /usr/bin/clang clang /usr/bin/clang-9 10 \
 update-alternatives --install /usr/lib/llvm llvm /usr/lib/llvm-9 20 \
                         --slave /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-9 \
                         --slave /usr/bin/llvm-link llvm-link /usr/bin/llvm-link-9
+
+
+wget https://go.dev/dl/go1.20.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.20.linux-amd64.tar.gz
+
+# revert bnutils version back to 2.26.1 for objcopy compatibility
+wget https://ftp.gnu.org/gnu/binutils/binutils-2.26.1.tar.gz
+tar -xzf binutils-2.26.1.tar.gz
+cd binutils-2.26.1
+./configure --prefix=/opt/binutils-2.26.1
+make -j$(nproc)
+sudo make install
+
+export PATH=/opt/binutils-2.26.1/bin:$PATH
+
